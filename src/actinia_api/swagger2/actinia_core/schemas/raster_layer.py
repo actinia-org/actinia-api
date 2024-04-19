43b879db776123554dd0d65666a6970798e55673
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2024-04-19 16:28:37 +0200

    add project property

--- src/actinia_api/swagger2/actinia_core/schemas/raster_layer.py
+++ src/actinia_api/swagger2/actinia_core/schemas/raster_layer.py
@@ -59,6 +59,7 @@ class RasterInfoModel(Schema):
         "location": {"type": "string"},
         "map": {"type": "string"},
         "mapset": {"type": "string"},
+        "project": "nc_spm_08",
         "rows": {"type": "string"},
         "source1": {"type": "string"},
         "north": {"type": "string"},
